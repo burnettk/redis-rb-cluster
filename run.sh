0e108f72c36fcf9fb3f2e52d6fa135d8779b5893
Kevin Burnett <burnettk@gmail.com>
2018-12-18 10:56:43 -0500

    fix host/port mismatch. w/ jasquat

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ fi
 
 if [ -z "$REDIS_RB_PORT" ]; then
   echo "INFO: you did not specify REDIS_RB_PORT environment variable. defaulting to 26379, the standard sentinel port"
-  REDIS_RB_HOST=26379
+  REDIS_RB_PORT=26379
 fi
 
 echo "Starting Redis test with start host $REDIS_RB_HOST at port $REDIS_RB_PORT"
